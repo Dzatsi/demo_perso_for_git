f765be721dc056b70f2002a8210573087b6e61e3
lenny dzatsi <lennydzatsi8@gmail.com>
2023-07-30 12:45:16 +0200

    add function and declared variable

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // my demo for git and Github
+
+const job = 'developper'
+function rappelName (leny, age){
+    if((typeof leny === 'string') && (typeof age === 'number')){
+        return `hi ${leny} !! you have ${age} years old and you're ${job}`
+    }else {
+    
+        return "syntax error"
+    }
+     
+}
+const nom = 'peter';
+// console.log(nom);
+const mbula = 27
+console.log(rappelName(nom, mbula));
